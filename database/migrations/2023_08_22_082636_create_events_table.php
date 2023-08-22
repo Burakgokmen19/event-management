6a8fe61ae6a051783fa42d41f553ea3cf170014e
Burakgokmen19 <burakgokmn19@gmail.com>
2023-08-22 16:31:10 +0300

    edit

--- database/migrations/2023_08_22_082636_create_events_table.php
+++ database/migrations/2023_08_22_082636_create_events_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->foreignIdFor(User::class);
             $table->string('name');
             $table->text('description');
-
             $table->dateTime('start_time');
             $table->dateTime('end_time');
 
