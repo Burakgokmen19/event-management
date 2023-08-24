8bf81eb7d378bcc0ab07bfcdee3228a4027483b3
Burakgokmen19 <burakgokmn19@gmail.com>
2023-08-24 13:06:55 +0300

    edit

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -20,8 +20,8 @@ class EventResource extends JsonResource
        'id'=> $this->id,
        'name' => $this->name,
        'description'=>$this->description,
-       'start_time'=>$this->start_time,
-        'end_time'=>$this->end_time,
+       'startTime'=>$this->start_time,
+        'endTime'=>$this->end_time,
             'user'=> new UserResource($this->whenLoaded('user')),
             'attendees'=> AttendeeResource::collection(
                 $this->whenLoaded('attendees'))
